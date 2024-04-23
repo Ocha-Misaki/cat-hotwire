c8b26e5792ac30fae1e6eedc2019ce1440da90ba
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-04-23 15:41:50 +0900

    form_controllerの読み込みを追加

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="form"
+export default class extends Controller {
+  submit() {
+    this.element.requestSubmit()
+    console.log('hello')
+  }
+}
