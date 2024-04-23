ccb75a23989c3ab4ce41b1e0c2fec15ec4001007
Ocha-Misaki <himawari.158fuji@gmail.com>
2024-04-23 16:59:55 +0900

    flashにはtoastを使用するように修正

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,8 @@ import FormController from "./form_controller.js"
 application.register("form", FormController)
 import ModalController from "./modal_controller.js"
 application.register("modal", ModalController)
+import ToastController from "./toast_controller.js"
+application.register("toast", ToastController)
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 // import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 // eagerLoadControllersFrom("controllers", application)
